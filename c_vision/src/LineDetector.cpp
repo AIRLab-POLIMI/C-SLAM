e9565e440ba389c6b13a353bc6da93b8493aa974
Boris_il_forte <davide.tateo90@gmail.com>
2014-06-27 16:20:53 +0200

    Deleted useless thinning
    - no major improvement, slowing down the algorithm a lot

--- c_vision/src/LineDetector.cpp
+++ c_vision/src/LineDetector.cpp
@@ -46,8 +46,6 @@ void LineDetector::detect(Mat& input, double roll)
 	double low_thres = high_thres * cannyP.alpha;
 	Canny(input, canny, low_thres, high_thres, cannyP.apertureSize, true);
 
-	thinning(canny, 1);
-
 	vector<Vec4i> lines;
 
 	HoughLinesP(canny, lines, houghP.rho, houghP.teta, houghP.threshold,
@@ -66,56 +64,3 @@ void LineDetector::detect(Mat& input, double roll)
 	viewer.display(colored);
 
 }
-
-void LineDetector::thinning(Mat& im, int maxIterations)
-{
-	int iterationCount = 0;
-	im /= 255;
-
-	Mat prev = Mat::zeros(im.size(), CV_8UC1);
-	Mat diff;
-
-	do
-	{
-		thinningIteration(im, 0);
-		thinningIteration(im, 1);
-		absdiff(im, prev, diff);
-		im.copyTo(prev);
-	} while (countNonZero(diff) > 0 && iterationCount++ < maxIterations);
-
-	im *= 255;
-}
-
-void LineDetector::thinningIteration(Mat& im, int iter)
-{
-	Mat marker = Mat::zeros(im.size(), CV_8UC1);
-
-	for (int i = 1; i < im.rows - 1; i++)
-	{
-		for (int j = 1; j < im.cols - 1; j++)
-		{
-			uchar p2 = im.at<uchar>(i - 1, j);
-			uchar p3 = im.at<uchar>(i - 1, j + 1);
-			uchar p4 = im.at<uchar>(i, j + 1);
-			uchar p5 = im.at<uchar>(i + 1, j + 1);
-			uchar p6 = im.at<uchar>(i + 1, j);
-			uchar p7 = im.at<uchar>(i + 1, j - 1);
-			uchar p8 = im.at<uchar>(i, j - 1);
-			uchar p9 = im.at<uchar>(i - 1, j - 1);
-
-			int A = (p2 == 0 && p3 == 1) + (p3 == 0 && p4 == 1)
-						+ (p4 == 0 && p5 == 1) + (p5 == 0 && p6 == 1)
-						+ (p6 == 0 && p7 == 1) + (p7 == 0 && p8 == 1)
-						+ (p8 == 0 && p9 == 1) + (p9 == 0 && p2 == 1);
-			int B = p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;
-			int m1 = iter == 0 ? (p2 * p4 * p6) : (p2 * p4 * p8);
-			int m2 = iter == 0 ? (p4 * p6 * p8) : (p2 * p6 * p8);
-
-			if (A == 1 && (B >= 2 && B <= 6) && m1 == 0 && m2 == 0)
-				marker.at<uchar>(i, j) = 1;
-		}
-	}
-
-	im &= ~marker;
-}
-
