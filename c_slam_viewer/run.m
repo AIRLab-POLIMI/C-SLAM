6e8373f5e4e72a5dd6959acaa537d5cced6a71db
davide <davide@fitzgerald.(none)>
2014-11-13 19:01:38 +0100

    added some stuff to automatically place the windows

--- c_slam_viewer/run.m
+++ c_slam_viewer/run.m
@@ -11,9 +11,17 @@ stopAll = 0;
 
 path = '/tmp/roamfree/';
 
+%close all
+%figure(1)
+%figure(2)
+%set(2, 'DefaultAxesPosition', [0.05, 0.05, 0.9, 0.9])
+
 close all
 figure(1)
-figure(2)
-set(2, 'DefaultAxesPosition', [0.05, 0.05, 0.9, 0.9])
+set(1,'OuterPosition', [0 0 1680/2 1050])
+
+figure(2)  
+set(2,'OuterPosition', [1680/2 0 1680/2 1050])
+
 
 watchFile();
