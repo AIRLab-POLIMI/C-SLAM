dfc8e9af7b45ce69844ee3f83657572de24a858c
Boris_il_forte <davide.tateo90@gmail.com>
2014-11-17 08:57:18 +0100

    Auto resolution pickup
    - always on screen 0
    - auto resolution choose instead of fixed one

--- c_slam_viewer/run.m
+++ c_slam_viewer/run.m
@@ -1,27 +1,26 @@
-addpath('lib')
+addpath('lib');
+
+res = get(0,'screensize');
 
 global path
 global stopAll
     
 stopAll = 1;
 
-pause(1)
+pause(1);
 
 stopAll = 0;
 
 path = '/tmp/roamfree/';
 
-%close all
-%figure(1)
-%figure(2)
-%set(2, 'DefaultAxesPosition', [0.05, 0.05, 0.9, 0.9])
+
 
 close all
 figure(1)
-set(1,'OuterPosition', [0 0 1680/2 1050])
+set(1,'OuterPosition', [0 0 res(3)/2 res(4)])
 
 figure(2)  
-set(2,'OuterPosition', [1680/2 0 1680/2 1050])
+set(2,'OuterPosition', [res(3)/2 0 res(3)/2 res(4)])
 
 
 watchFile();
