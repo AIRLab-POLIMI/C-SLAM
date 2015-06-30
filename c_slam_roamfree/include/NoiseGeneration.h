5c49ffc1d45d3c1624afb468d19634e04b402c51
boris-il-forte <boris.ilpossente@hotmail.it>
2015-06-30 10:29:24 +0200

    Added missing file
    - simple file that makes the build failing

---
+++ c_slam_roamfree/include/NoiseGeneration.h
@@ -0,0 +1,30 @@
+/*
+ * c_slam_roamfree,
+ *
+ *
+ * Copyright (C) 2015 Davide Tateo
+ * Versione 1.0
+ *
+ * This file is part of c_slam_roamfree.
+ *
+ * c_slam_roamfree is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * c_slam_roamfree is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with c_slam_roamfree.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_NOISEGENERATION_H_
+#define INCLUDE_NOISEGENERATION_H_
+
+void add_gaussian_noise(double *to, unsigned int size, double mean, double std);
+
+
+#endif /* INCLUDE_NOISEGENERATION_H_ */
